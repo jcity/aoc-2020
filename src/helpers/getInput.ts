bdcc0cdf668003933af822a3ab63cbddc933715e
Justin Myers <justin@convoy.com>
2020-12-02 17:24:32 -0800

    Trim incase of trailing newline when getting input

--- src/helpers/getInput.ts
+++ src/helpers/getInput.ts
@@ -2,6 +2,6 @@ import fs from 'fs';
 import path from 'path';
 
 export const getInput = async (dayNumber: number) => {
-  const input = fs.readFileSync(path.resolve(`${__dirname}/../../input/day${dayNumber}.txt`), 'utf-8');
+  const input = fs.readFileSync(path.resolve(`${__dirname}/../../input/day${dayNumber}.txt`), 'utf-8').trim();
   return input.split('\n');
 };
