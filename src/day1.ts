2c1d431bcc1319832d8a53536c4f409630f4f25d
Justin Myers <justin@convoy.com>
2020-12-02 17:45:27 -0800

    Day 1 cleanup

--- src/day1.ts
+++ src/day1.ts
@@ -1,67 +1,61 @@
 import { getInput } from './helpers/getInput';
 
+const TARGET = 2020;
+const INPUT = getInput(1)
+  .map((num: string) => +num)
+  .sort((n1, n2) => {
+    if (n1 < n2) return -1;
+    if (n1 > n2) return 1;
+    return 0;
+  });
+
 // find the two entries that sum to 2020 and then multiply those two numbers together
-const part1 = (input: number[]) => {
+const part1 = (numbers: number[]) => {
   console.log('Part 1');
-  const target = 2020;
   let low = 0;
-  let high = input.length - 1;
-  while (input[low] + input[high] !== target && low <= high) {
-    if (input[low] + input[high] < target) {
+  let high = numbers.length - 1;
+  while (numbers[low] + numbers[high] !== TARGET && low <= high) {
+    if (numbers[low] + numbers[high] < TARGET) {
       low += 1;
     } else {
       high -= 1;
     }
   }
 
-  console.log(`Low: ${input[low]}`);
-  console.log(`High: ${input[high]}`);
-  console.log(`Sum: ${input[low] + input[high]}`);
-  console.log(`Answer: ${input[low] * input[high]}`);
+  console.log(`Low: ${numbers[low]}`);
+  console.log(`High: ${numbers[high]}`);
+  console.log(`Sum: ${numbers[low] + numbers[high]}`);
+  console.log(`Answer: ${numbers[low] * numbers[high]}`);
 };
 
 // find the three entries that sum to 2020 and then multiply those three numbers together
-const part2 = (input: number[]) => {
+const part2 = (numbers: number[]) => {
   console.log('\nPart 2');
-  const finalTarget = 2020;
-  let high = input.length - 1;
-  while (high > finalTarget) high -= 1;
-
   let low = 0;
   let mid = 1;
-  while (input[low] + input[mid] + input[high] !== finalTarget) {
-    const localTarget = finalTarget - input[high];
-    while (input[low] + input[mid + 1] <= localTarget && mid < high) {
+  let high = numbers.length - 1;
+  while (numbers[low] + numbers[mid] + numbers[high] !== TARGET) {
+    const localTarget = TARGET - numbers[high];
+    while (numbers[low] + numbers[mid + 1] <= localTarget && mid < high) {
       mid += 1;
     }
-    while (input[low + 1] + input[mid] <= localTarget && low < mid) {
+    while (numbers[low + 1] + numbers[mid] <= localTarget && low < mid) {
       low += 1;
     }
 
-    if (input[low] + input[mid] !== localTarget) {
+    if (numbers[low] + numbers[mid] !== localTarget) {
       high -= 1;
       low = 0;
       mid = 1;
     }
   }
 
-  console.log(`Low: ${input[low]}`);
-  console.log(`Mid: ${input[mid]}`);
-  console.log(`High: ${input[high]}`);
-  console.log(`Sum: ${input[low] + input[mid] + input[high]}`);
-  console.log(`Answer: ${input[low] * input[mid] * input[high]}`);
+  console.log(`Low: ${numbers[low]}`);
+  console.log(`Mid: ${numbers[mid]}`);
+  console.log(`High: ${numbers[high]}`);
+  console.log(`Sum: ${numbers[low] + numbers[mid] + numbers[high]}`);
+  console.log(`Answer: ${numbers[low] * numbers[mid] * numbers[high]}`);
 };
 
-const run = async () => {
-  const input = (await getInput(1))
-    .map((num: string) => +num)
-    .sort((n1, n2) => {
-      if (n1 < n2) return -1;
-      if (n1 > n2) return 1;
-      return 0;
-    });
-  part1(input);
-  part2(input);
-};
-
-run();
+part1(INPUT);
+part2(INPUT);
--- src/helpers/getInput.ts
+++ src/helpers/getInput.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export const getInput = async (dayNumber: number) => {
+export const getInput = (dayNumber: number) => {
   const input = fs.readFileSync(path.resolve(`${__dirname}/../../input/day${dayNumber}.txt`), 'utf-8').trim();
   return input.split('\n');
 };
