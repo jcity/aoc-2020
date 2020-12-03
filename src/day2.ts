9a844d596a1e96cba36b92321169e7dd48f60688
Justin Myers <justin@convoy.com>
2020-12-02 18:06:01 -0800

    Day 2 Part 1

--- src/day2.ts
+++ src/day2.ts
@@ -1,1 +1,20 @@
-console.log('day 2');
+import { getInput } from './helpers/getInput';
+
+const INPUT = getInput(2).map((line: string) => {
+  const [bounds, letter, password] = line.split(' ');
+  const [min, max] = bounds.split('-');
+  return {
+    letter: letter.replace(':', ''),
+    max: +max,
+    min: +min,
+    password,
+  };
+});
+
+const part1 = INPUT.filter((testCase) => {
+  const count = testCase.password.split('').filter((letter) => letter === testCase.letter).length;
+  return count >= testCase.min && count <= testCase.max;
+}).length;
+
+console.log('Part 1');
+console.log(`Answer: ${part1}`);
