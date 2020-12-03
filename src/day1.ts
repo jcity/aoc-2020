0ef0e2eee8b6e24cb6f4d43c1933cc8c5866455b
Justin Myers <justin@convoy.com>
2020-12-02 17:25:31 -0800

    Day 1 Part 2

--- src/day1.ts
+++ src/day1.ts
@@ -1,6 +1,57 @@
 import { getInput } from './helpers/getInput';
 
 // find the two entries that sum to 2020 and then multiply those two numbers together
+const part1 = (input: number[]) => {
+  console.log('Part 1');
+  const target = 2020;
+  let low = 0;
+  let high = input.length - 1;
+  while (input[low] + input[high] !== target && low <= high) {
+    if (input[low] + input[high] < target) {
+      low += 1;
+    } else {
+      high -= 1;
+    }
+  }
+
+  console.log(`Low: ${input[low]}`);
+  console.log(`High: ${input[high]}`);
+  console.log(`Sum: ${input[low] + input[high]}`);
+  console.log(`Answer: ${input[low] * input[high]}`);
+};
+
+// find the three entries that sum to 2020 and then multiply those three numbers together
+const part2 = (input: number[]) => {
+  console.log('\nPart 2');
+  const finalTarget = 2020;
+  let high = input.length - 1;
+  while (high > finalTarget) high -= 1;
+
+  let low = 0;
+  let mid = 1;
+  while (input[low] + input[mid] + input[high] !== finalTarget) {
+    const localTarget = finalTarget - input[high];
+    while (input[low] + input[mid + 1] <= localTarget && mid < high) {
+      mid += 1;
+    }
+    while (input[low + 1] + input[mid] <= localTarget && low < mid) {
+      low += 1;
+    }
+
+    if (input[low] + input[mid] !== localTarget) {
+      high -= 1;
+      low = 0;
+      mid = 1;
+    }
+  }
+
+  console.log(`Low: ${input[low]}`);
+  console.log(`Mid: ${input[mid]}`);
+  console.log(`High: ${input[high]}`);
+  console.log(`Sum: ${input[low] + input[mid] + input[high]}`);
+  console.log(`Answer: ${input[low] * input[mid] * input[high]}`);
+};
+
 const run = async () => {
   const input = (await getInput(1))
     .map((num: string) => +num)
@@ -9,17 +60,8 @@ const run = async () => {
       if (n1 > n2) return 1;
       return 0;
     });
-
-  const target = 2020;
-  let left = 0;
-  let right = input.length - 1;
-  while (input[left] + input[right] !== target && input[left] <= input[right]) {
-    if (input[left] + input[right] < target) left += 1;
-    if (input[left] + input[right] > target) right -= 1;
-  }
-  console.log(`Left: ${input[left]}`);
-  console.log(`Right: ${input[right]}`);
-  console.log(`Answer: ${input[left] * input[right]}`);
+  part1(input);
+  part2(input);
 };
 
 run();
