f7f140c9db83ab0d5ecd098976d1a060c0a23d44
Justin Myers <justin@convoy.com>
2020-12-02 16:16:20 -0800

    Day 1 Part 1 complete

--- src/day1.ts
+++ src/day1.ts
@@ -1,1 +1,25 @@
-console.log('day 1');
+import { getInput } from './helpers/getInput';
+
+// find the two entries that sum to 2020 and then multiply those two numbers together
+const run = async () => {
+  const input = (await getInput(1))
+    .map((num: string) => +num)
+    .sort((n1, n2) => {
+      if (n1 < n2) return -1;
+      if (n1 > n2) return 1;
+      return 0;
+    });
+
+  const target = 2020;
+  let left = 0;
+  let right = input.length - 1;
+  while (input[left] + input[right] !== target && input[left] <= input[right]) {
+    if (input[left] + input[right] < target) left += 1;
+    if (input[left] + input[right] > target) right -= 1;
+  }
+  console.log(`Left: ${input[left]}`);
+  console.log(`Right: ${input[right]}`);
+  console.log(`Answer: ${input[left] * input[right]}`);
+};
+
+run();
