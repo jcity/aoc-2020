6f06d20b07e3985892e7bb0956008d607f634159
Justin Myers <justin@convoy.com>
2020-12-08 13:36:51 -0800

    Day 4 Part 2

--- src/day4.ts
+++ src/day4.ts
@@ -13,12 +13,38 @@ const INPUT = getInput(4).reduce<string[][]>(
   [[]],
 );
 
-const part1 = INPUT.filter((value: string[]) => {
-  if (value.length === 8) return true;
-  if (value.length <= 6) return false;
-  const keys = value.map((val) => val.split(':')[0]);
+const part1 = INPUT.filter((fields: string[]) => {
+  if (fields.length === 8) return true;
+  if (fields.length <= 6) return false;
+  const keys = fields.map((value) => value.split(':')[0]);
   return !keys.includes('cid');
-}).length;
+});
+
+const isFourDigits = (s: string) => s.match(/^\d{4}$/);
+const inRange = (n: number, l: number, h: number) => n >= l && n <= h;
+
+const part2 = part1.filter((fields: string[]) => {
+  const validFields = fields.filter((value) => {
+    const [key, val] = value.split(':');
+    if (key === 'byr') return isFourDigits(val) && inRange(+val, 1920, 2002);
+    if (key === 'iyr') return isFourDigits(val) && inRange(+val, 2010, 2020);
+    if (key === 'eyr') return isFourDigits(val) && inRange(+val, 2020, 2030);
+    if (key === 'hgt') {
+      const unit = val.slice(-2);
+      if (unit === 'cm') return inRange(+val.replace('cm', ''), 150, 193);
+      if (unit === 'in') return inRange(+val.replace('in', ''), 59, 76);
+      return false;
+    }
+    if (key === 'hcl') return val.match(/^#(\d|[a-f]){6}$/);
+    if (key === 'ecl') return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(val);
+    if (key === 'pid') return val.match(/^\d{9}$/);
+    return true; // cid
+  });
+  return fields.length === validFields.length;
+});
 
 console.log('Part 1');
-console.log(`Solution: ${part1}`);
+console.log(`Solution: ${part1.length}`);
+
+console.log('Part 2');
+console.log(`Solution: ${part2.length}`);
