5f01fa61d9d8ce681ea811cb70acbf2198666078
Justin Myers <justin@convoy.com>
2020-12-08 14:07:03 -0800

    Day 4 cleanup

--- src/day4.ts
+++ src/day4.ts
@@ -1,50 +1,32 @@
 import { getInput } from './helpers/getInput';
 
-const INPUT = getInput(4).reduce<string[][]>(
-  (lastValue: string[][], curValue: string) => {
-    if (curValue === '') {
-      lastValue.push([]);
-    } else {
-      const idx = lastValue.length - 1;
-      curValue.split(' ').forEach((val: string) => lastValue[idx].push(val));
-    }
-    return lastValue;
-  },
-  [[]],
-);
+const INPUT = getInput(4, '\n\n').map((line) => line.split(/\s+/).map((field) => field.split(':')));
 
-const part1 = INPUT.filter((fields: string[]) => {
-  if (fields.length === 8) return true;
-  if (fields.length <= 6) return false;
-  const keys = fields.map((value) => value.split(':')[0]);
-  return !keys.includes('cid');
+const part1 = INPUT.filter((passportFields: string[][]) => {
+  if (passportFields.length === 8) return true;
+  if (passportFields.length <= 6) return false;
+  return passportFields.filter(([key]) => key === 'cid').length === 0;
 });
 
-const isFourDigits = (s: string) => s.match(/^\d{4}$/);
 const inRange = (n: number, l: number, h: number) => n >= l && n <= h;
-
-const part2 = part1.filter((fields: string[]) => {
-  const validFields = fields.filter((value) => {
-    const [key, val] = value.split(':');
-    if (key === 'byr') return isFourDigits(val) && inRange(+val, 1920, 2002);
-    if (key === 'iyr') return isFourDigits(val) && inRange(+val, 2010, 2020);
-    if (key === 'eyr') return isFourDigits(val) && inRange(+val, 2020, 2030);
-    if (key === 'hgt') {
-      const unit = val.slice(-2);
-      if (unit === 'cm') return inRange(+val.replace('cm', ''), 150, 193);
-      if (unit === 'in') return inRange(+val.replace('in', ''), 59, 76);
+const part2 = part1.filter((passportFields: string[][]) => {
+  const validFields = passportFields.filter(([field, value]: string[]) => {
+    if (field === 'byr') return inRange(+value, 1920, 2002);
+    if (field === 'iyr') return inRange(+value, 2010, 2020);
+    if (field === 'eyr') return inRange(+value, 2020, 2030);
+    if (field === 'hgt') {
+      const unit = value.slice(-2);
+      if (unit === 'in') return inRange(+value.replace('in', ''), 59, 76);
+      if (unit === 'cm') return inRange(+value.replace('cm', ''), 150, 193);
       return false;
     }
-    if (key === 'hcl') return val.match(/^#(\d|[a-f]){6}$/);
-    if (key === 'ecl') return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(val);
-    if (key === 'pid') return val.match(/^\d{9}$/);
+    if (field === 'hcl') return value.match(/^#(\d|[a-f]){6}$/);
+    if (field === 'ecl') return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
+    if (field === 'pid') return value.match(/^\d{9}$/);
     return true; // cid
   });
-  return fields.length === validFields.length;
+  return validFields.length === passportFields.length;
 });
 
-console.log('Part 1');
-console.log(`Solution: ${part1.length}`);
-
-console.log('Part 2');
-console.log(`Solution: ${part2.length}`);
+console.log(`Part 1 Solution: ${part1.length}`);
+console.log(`Part 2 Solution: ${part2.length}`);
