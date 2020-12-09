62e7ccc1e6b9519cb972b8d0136f24bee9b3afe1
Justin Myers <justin@convoy.com>
2020-12-08 16:08:35 -0800

    Day 6 Part 2

--- src/day6.ts
+++ src/day6.ts
@@ -2,12 +2,24 @@ import { getInput } from './helpers/getInput';
 
 const INPUT = getInput(6, '\n\n').map((line) => line.split('\n'));
 
-const part1 = INPUT.map((answers) => {
-  const questions = new Set();
-  answers.forEach((answer) => {
-    answer.split('').forEach((letter) => questions.add(letter));
-  });
-  return questions.size;
-}).reduce((prev, cur) => prev + cur);
+const groupAnswers = INPUT.map((group) => group.map((answers) => new Set(answers.split(''))));
+
+const part1 = groupAnswers
+  .map((answers) => {
+    return answers.reduce((prev, cur) => new Set([...prev.values(), ...cur.values()])).size;
+  })
+  .reduce((prev, cur) => prev + cur);
+
+const part2 = groupAnswers
+  .map((answers) => {
+    return answers.reduce((prev, cur) => {
+      prev.forEach((questionId) => {
+        if (!cur.has(questionId)) prev.delete(questionId);
+      });
+      return prev;
+    }).size;
+  })
+  .reduce((prev, cur) => prev + cur);
 
 console.log(`Part 1 Solution: ${part1}`);
+console.log(`Part 2 Solution: ${part2}`);
